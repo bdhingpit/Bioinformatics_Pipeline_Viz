17bd83570b5fc7c9c80a2e8f5951911085bb1ea1
bdhingpit <bdhingpit@gmail.com>
2024-12-26 16:25:41 +0800

    Updated repo

--- utils/parse_nodes_and_edges.py
+++ utils/parse_nodes_and_edges.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 utils_dir = os.path.dirname(os.path.abspath(__file__))
-tools_list_filepath = os.path.join(utils_dir, '..', 'pipeline', 'tools_list.xml')
+tools_list_filepath = os.path.join(utils_dir, '..', 'pipeline', 'tools_list2.xml')
 
 tree = ET.parse(tools_list_filepath)
 root = tree.getroot()
@@ -17,39 +17,43 @@ def load_pipeline_file(pipeline_filename):
 
 
 def generate_edges(pipeline_filename):
+    """
+    Create dictionary of edges.
+    """
     pipeline_df = load_pipeline_file(pipeline_filename)
 
     edges = []
 
     for idx, row in pipeline_df.iterrows():
-        source_split = row['source'].split('.')
-        # target_split = row['target'].split('.')
+        source_split = row['source'].split('.', maxsplit=1)
+        tool_output_split = source_split[1].rsplit('.', maxsplit=1)  # Some tool names have '.'
 
         # If the source data is not an output of previous step in workflow (i.e. external node)
-        if source_split[1] == 'ext_data':
+        if tool_output_split[0] == 'ext_data':
             edges.append({'data': {'source': row['source'], 'target': row['target']}})
 
         # If the source data is an output of a previous step in the workflow
         else:
-            output_files_list = get_output_text(source_split[1])
+            output_files_list = get_output_text(tool_output_split[0])
 
-            for output_num in source_split[2].split(','):
-                file_output_src = (source_split[0], source_split[1], output_files_list[int(output_num) - 1])
+            for output_num in tool_output_split[1].split(','):
+                file_output_src = (source_split[0], tool_output_split[0], output_files_list[int(output_num) - 1])
                 edges.append({'data': {'source': '.'.join(file_output_src), 'target': row['target']}})
 
-    # print('===== EDGES =====')
-    # print(edges)
     return edges
 
 
 def generate_nodes(pipeline_filename):
+    """
+    Create dictionary of nodes
+    """
     pipeline_df = load_pipeline_file(pipeline_filename)
 
     nodes = []
 
     for idx, row in pipeline_df.iterrows():
         source_split = row['source'].split('.')
-        target_split = row['target'].split('.')
+        target_split = row['target'].split('.', maxsplit=1)
 
         # For the initial file/s (i.e. external nodes)
         if source_split[1] == 'ext_data':
@@ -64,8 +68,6 @@ def generate_nodes(pipeline_filename):
         for output_file in output_files_list:
             nodes.append({'data': {'id': row['target']+'.'+output_file, 'parent': row['target']}})
 
-    # print("===== NODES =====")
-    # print(nodes)
     return nodes
 
 
--- utils/parse_pipeline_text.py
+++ utils/parse_pipeline_text.py
@@ -4,19 +4,22 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html
 from utils import parse_nodes_and_edges as pne
 
-
 utils_dir = os.path.dirname(os.path.abspath(__file__))
 pipeline_list_filepath = os.path.join(utils_dir, '..', 'pipeline', 'pipeline_list.xml')
 
 pipeline_tree = ET.parse(pipeline_list_filepath)
 pipeline_root = pipeline_tree.getroot()
 
-tools_list_filepath = os.path.join(utils_dir, '..', 'pipeline', 'tools_list.xml')
+tools_list_filepath = os.path.join(utils_dir, '..', 'pipeline', 'tools_list2.xml')
 tools_tree = ET.parse(tools_list_filepath)
 tools_root = tools_tree.getroot()
 
 
 def get_workflow_details_from_filename(filename):
+    """
+    Get workflow name and description from the file associated
+    with the selected pipeline.
+    """
     if filename is not None:
         for parent in pipeline_root.findall('workflow'):
             for child in parent:
@@ -29,14 +32,25 @@ def get_workflow_details_from_filename(filename):
         return None
 
 
-# There are three types of nodes:
-# (1) Node referring to externally generated files. Labeled: "0.ext_data.*"
-# (2) Parent nodes or tools used in the pipeline
-# (3) Children nodes or outputs of tools used in the pipeline
-# From hereon, node types (2) and (3) will be called "internal nodes"
-# and node type (1) will be called "external node"
+"""
+There are three types of nodes:
+(1) Node referring to externally generated files. Labeled: "0.ext_data.*"
+(2) Parent nodes or tools used in the pipeline
+(3) Children nodes or outputs of tools used in the pipeline
+From hereon, node types (2) and (3) will be called "internal nodes"
+and node type (1) will be called "external node"
+"""
+
+
 def get_internal_node_desc(node):
-    tool_name = node['id'].split('.')[1]
+    """
+    Get details for selected internal node.
+    """
+    # Get tool name
+    if 'label' in node.keys():  # If parent node is clicked
+        tool_name = node['label']
+    else:  # If internal node is clicked
+        tool_name = node['parent'].split('.', maxsplit=1)[1]
 
     for tool in tools_root.iter('tool'):
         # If tool
@@ -71,6 +85,9 @@ def get_internal_node_desc(node):
 
 
 def generate_internal_node_desc_accordion(node):
+    """
+    Create an accordion element for the selected internal node.
+    """
     tool_id, tool_name, tool_desc, out_type_list, out_desc_list = get_internal_node_desc(node)
 
     accordion_div = html.Div(
@@ -79,8 +96,8 @@ def generate_internal_node_desc_accordion(node):
             children=[
                 dbc.AccordionItem(
                     item_id=tool_id,
-                    children=['Description: '+tool_desc],
-                    title='Tool: '+tool_name
+                    children=['Description: ' + tool_desc],
+                    title='Tool: ' + tool_name
                 ),
                 dbc.AccordionItem(
                     item_id='tool-output',
@@ -89,8 +106,8 @@ def generate_internal_node_desc_accordion(node):
                             id='tool-output-inner-accordion',
                             children=[
                                 dbc.AccordionItem(
-                                    'Description: '+out_desc,
-                                    item_id=tool_id+'.'+out_type,
+                                    'Description: ' + out_desc,
+                                    item_id=tool_id + '.' + out_type,
                                     title=out_type,
                                     style={
                                         'border-style': 'dashed'
@@ -112,6 +129,9 @@ def generate_internal_node_desc_accordion(node):
 
 
 def generate_external_node_accordion(node, filename):
+    """
+    Create an accordion for the selected external node
+    """
     accordion_div = html.Div(
         children=[
             dbc.Accordion(
@@ -120,7 +140,7 @@ def generate_external_node_accordion(node, filename):
                     dbc.AccordionItem(
                         item_id=node['id'],
                         children=[retrieve_external_node_desc(node, filename)],
-                        title='External Input Data: '+node['id'].split('.')[2]
+                        title='External Input Data: ' + node['id'].split('.')[2]
                     )
                 ],
                 start_collapsed=True,
@@ -137,21 +157,25 @@ def generate_external_node_accordion(node, filename):
 
 
 def retrieve_external_node_desc(node, filename):
+    """
+    Retrieve information of the external node from the *pipeline.txt file
+    """
     edge_list = pne.generate_edges(filename)
     ext_node_type = node['id'].split('.')[2]
+    pipeline_df = pne.load_pipeline_file(filename)
 
-    for edge in edge_list:
-        if edge['data']['source'] == node['id']:
-            target = edge['data']['target']
-            tool = tools_root.find("./tool[@name='{}']".format(target.split('.')[1]))
-            ext_input = tool.find("./ext-input/[input-type='{}']".format(ext_node_type))
+    for idx, row in pipeline_df.iterrows():
+        if row['source'] == node['id']:
+            return row['ext_data_desc']
 
-            ext_node_type = ext_input.find('input-type').text
-            ext_node_desc = ext_input.find('input-desc').text
-
-            return ext_node_desc
-
-
-
+    # for edge in edge_list:
+    #     if edge['data']['source'] == node['id']:
+    #         target = edge['data']['target']
+    #         tool = tools_root.find("./tool[@name='{}']".format(target.split('.')[1]))
+    #         ext_input = tool.find("./ext-input/[input-type='{}']".format(ext_node_type))
+    #
+    #         ext_node_desc = ext_input.find('input-desc').text
+    #
+    #         return ext_node_desc
 
 # generate_node_desc_accordion({'id': '3.qiime2_vsearch_merge-pairs', 'label': 'qiime2_vsearch_merge-pairs', 'timeStamp': 1721821099141})
